b3c8e78b6bd0b87aee160d75a965133542ccd856
nikitaraut2232 <nikitaraut2232@gmail.com>
2024-04-01 18:55:29 +0530

    create Gallery  Page

--- src/components/common_components/Galleryimages.js
+++ src/components/common_components/Galleryimages.js
@@ -1,6 +1,11 @@
 import './Galleryimages.css'
 import { useState } from 'react'
-import FontAwesome from 'react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import{
+    faCircleXmark,
+    faCircleChevronLeft,
+    faCircleChevronRight
+} from '@fortawesome/free-solid-svg-icons' 
  const Galleryimages =({galleryImages})=>{
     const [slideNumber,  setSlideNumber]= useState(0)
     const [openModel, setOpenModel]= useState(false)
@@ -8,12 +13,29 @@ import FontAwesome from 'react-fontawesome'
         setSlideNumber(index)
         setOpenModel(true)
     }
+    const handalCloseModel=()=>{
+          setOpenModel(false)
+    }
+    const prevSlide = ()=>{
+             slideNumber == 0 ? setSlideNumber( galleryImages.length-1): setSlideNumber(slideNumber-1)
+    }
+    const nextSlide = ()=>{
+        slideNumber == 0 ? setSlideNumber( galleryImages.length-1): setSlideNumber(slideNumber-1)    
+    }
     return(
         <div className='mt-5'>
+            
+            
             {
                 openModel && 
                 <div className='sliderwrap'> 
-                
+                  <FontAwesomeIcon icon={ faCircleXmark }   className='btnclose' onClick={handalCloseModel}/>
+                  <FontAwesomeIcon icon={ faCircleChevronLeft } className='btnprev' onClick={prevSlide}/>
+                  <FontAwesomeIcon icon={ faCircleChevronRight }  className='btnnext' onClick={nextSlide}/>
+                <div className='fullscreenImage'>
+                    <img src={ galleryImages[slideNumber].img }  alt=''/>
+                </div>
+                 
                 </div>
             }
 
@@ -23,7 +45,7 @@ import FontAwesome from 'react-fontawesome'
                         return(
                             <div className='single'  key={index}
                             onClick={()=>handalOpenModel(index)}>
-                              <img src={slide.img} alt='' />
+                              <img  className='img_hei'src={slide.img} alt='' />
                             </div>
                         )
                     })
